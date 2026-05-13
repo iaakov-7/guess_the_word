c78485f9771f28c8957cdee2155a8ebb2eded0c6
yaakovizel <z7685727@gmail.com>
2026-05-13 16:52:47 +0300

    initial game version

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from random import choice
-
+from time import sleep
 def get_random_word():
     words_list = ["banana","coffee","summer","winter","family",
                   "yello","school", "guitar","garden","doctor","computer",
@@ -18,5 +18,76 @@ def user_guess():
         else:
             running = False
     return user_letter
+
+def show_word_status(word_in_lst_version):
+    return " ".join(word_in_lst_version)
     
+def game():
+    attempts = 7
+    current_attempts = 7
+    secret_word = get_random_word()     
+    word_with_lines = ["_"] * len(secret_word)
+    used_letters = ""
+    print(secret_word,show_word_status(word_with_lines))
+    while current_attempts:
+        user_letter = user_guess()
+        if user_letter in used_letters:
+            print(f"""you used this letter! 
+all used letters: {", ".join(used_letters)}""")
+            continue
+        used_letters += user_letter
+        letter_and_indexes = {}
+        flag = False
+        for i, letter in enumerate(secret_word):
+            if user_letter == letter:
+                letter_and_indexes[i] = letter
+                flag = True
+                   
+        if flag == False:
+            current_attempts -= 1
+            print(f"""booz, wrong guess! 
+You have {current_attempts} attempts left
+Status of word is {show_word_status(word_with_lines)}""")
+            
+        else:    
+            for i, letter in letter_and_indexes.items():
+                word_with_lines[i] = letter
+                if "_" not in word_with_lines:
+                    return f"""Congratulations, you guessed the whole word: '[{secret_word}]' 
+you used {attempts - current_attempts} from {attempts} attempts"""  
+            print(f"""Successful guess 
+the word now is: {show_word_status(word_with_lines)}""") 
+    return f"You failed, The game is over because you used all attempts the word was: '[{secret_word}]'"          
+    
+    
+def menu():
+    running = True
+    while running:
+        try:
+            action = int(input("""--------------------------------
+Welcome to 'guess the word' game
+--------------------------------
+   press 1 to start the game 
+   press 2 to exit 
+        : """))
+            return action
+        except ValueError:
+            print("Enter 1 or 2 only!")
+            
+def main():
+    running = True
+    while running:
+       choice = menu()
+       match choice:
+            case 1:
+                print(game())
+                sleep(3)
+            case 2:
+               print("bye...")
+               running = False
+            case _:
+                print("Enter 1 or 2 only!")
+
+main()
+
 
