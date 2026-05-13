59147068c711eb5229953e0827b7e2a994888874
yaakovizel <z7685727@gmail.com>
2026-05-13 17:45:22 +0300

    An improved version but there is still room for improvement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from random import choice
 from time import sleep
+
+
 def get_random_word():
     words_list = ["banana","coffee","summer","winter","family",
                   "yello","school", "guitar","garden","doctor","computer",
@@ -9,11 +11,11 @@ def get_random_word():
     return secret_word
 
 def user_guess():
-    abc_lettrs = [chr(i) for i in range(97,123)]
+    abc_letters = [chr(i) for i in range(97,123)]
     running = True
     while running:
         user_letter = input("Enter your guess (one Englisg letter)\n").lower()
-        if not user_letter in abc_lettrs:
+        if not user_letter in abc_letters:
             print("One English letter only!") 
         else:
             running = False
@@ -21,14 +23,14 @@ def user_guess():
 
 def show_word_status(word_in_lst_version):
     return " ".join(word_in_lst_version)
-    
+
 def game():
     attempts = 7
-    current_attempts = 7
+    current_attempts = attempts
     secret_word = get_random_word()     
     word_with_lines = ["_"] * len(secret_word)
     used_letters = ""
-    print(secret_word,show_word_status(word_with_lines))
+    print(show_word_status(word_with_lines))
     while current_attempts:
         user_letter = user_guess()
         if user_letter in used_letters:
@@ -36,30 +38,26 @@ def game():
 all used letters: {", ".join(used_letters)}""")
             continue
         used_letters += user_letter
-        letter_and_indexes = {}
         flag = False
         for i, letter in enumerate(secret_word):
             if user_letter == letter:
-                letter_and_indexes[i] = letter
                 flag = True
-                   
-        if flag == False:
+                word_with_lines[i] = letter
+                
+                if "_" not in word_with_lines:
+                    return f"""Congratulations, you guessed the whole word: '[{secret_word}]' 
+you used {attempts - current_attempts} from {attempts} attempts"""  
+        if flag == True:    
+            print(f"""Successful guess 
+the word now is: {show_word_status(word_with_lines)}""") 
+    
+        else:
             current_attempts -= 1
             print(f"""booz, wrong guess! 
 You have {current_attempts} attempts left
 Status of word is {show_word_status(word_with_lines)}""")
-            
-        else:    
-            for i, letter in letter_and_indexes.items():
-                word_with_lines[i] = letter
-                if "_" not in word_with_lines:
-                    return f"""Congratulations, you guessed the whole word: '[{secret_word}]' 
-you used {attempts - current_attempts} from {attempts} attempts"""  
-            print(f"""Successful guess 
-the word now is: {show_word_status(word_with_lines)}""") 
-    return f"You failed, The game is over because you used all attempts the word was: '[{secret_word}]'"          
-    
-    
+    return f"You failed, The game is over because you used all attempts the word was: '[{secret_word}]'"    
+
 def menu():
     running = True
     while running:
@@ -70,24 +68,20 @@ Welcome to 'guess the word' game
    press 1 to start the game 
    press 2 to exit 
         : """))
-            return action
+
+            match action:
+                case 1:
+                    print(game())
+                    sleep(3)
+                case 2:
+                    print("bye...")
+                    running = False
+                case _:
+                     print("Enter 1 or 2 only!")   
+            
         except ValueError:
             print("Enter 1 or 2 only!")
             
-def main():
-    running = True
-    while running:
-       choice = menu()
-       match choice:
-            case 1:
-                print(game())
-                sleep(3)
-            case 2:
-               print("bye...")
-               running = False
-            case _:
-                print("Enter 1 or 2 only!")
-
-main()
+menu()
 
 
