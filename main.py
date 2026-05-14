1b1c176d5a9a458e6a2939974b2733a1318727bf
yaakovizel <z7685727@gmail.com>
2026-05-14 12:13:36 +0300

    added version divided into functions

--- main.py
+++ main.py
@@ -12,50 +12,66 @@ def get_random_word():
 
 def user_guess():
     abc_letters = [chr(i) for i in range(97,123)]
-    running = True
-    while running:
+    user_letter = input("Enter your guess (one Englisg letter)\n").lower()
+    while not user_letter in abc_letters:
+        print("One English letter only!")
         user_letter = input("Enter your guess (one Englisg letter)\n").lower()
-        if not user_letter in abc_letters:
-            print("One English letter only!") 
-        else:
-            running = False
     return user_letter
 
 def show_word_status(word_in_lst_version):
-    return " ".join(word_in_lst_version)
+    return f"the status of word is:{" ".join(word_in_lst_version)}"
 
-def game():
+def show_used_letters(used_letters):
+    return f"the used words are: {" ,".join(used_letters)}"
+
+def show_how_many_attempts_left(current_attempts):
+    return f"you have left {current_attempts} attempts"
+     
+def show_status_game(masked_word,current_attempts,used_letters):
+    return f"""{show_word_status(masked_word)}
+    {show_how_many_attempts_left(current_attempts)}
+    {show_used_letters(used_letters)}"""
+
+def check_user_guess(user_letter,secret_word):
+    return user_letter in secret_word
+
+def update_masked_word(user_letter,masked_word,secret_word):
+    for i, letter in enumerate(secret_word):
+            if user_letter == letter:
+                masked_word[i] = letter 
+
+def check_victory(masked_word):
+    return "_" not in masked_word
+
+def check_if_used_letter(user_letter,used_letters):
+    return user_letter in used_letters                                           
+
+def game_management():
     attempts = 7
     current_attempts = attempts
     secret_word = get_random_word()     
-    word_with_lines = ["_"] * len(secret_word)
+    masked_word = ["_"] * len(secret_word)
     used_letters = ""
-    print(show_word_status(word_with_lines))
+    print(show_word_status(masked_word))
     while current_attempts:
         user_letter = user_guess()
-        if user_letter in used_letters:
-            print(f"""you used this letter! 
-all used letters: {", ".join(used_letters)}""")
+        if check_if_used_letter(user_letter,used_letters):
+            print(f"""you used this letter
+{show_used_letters(used_letters)}""")
             continue
         used_letters += user_letter
-        flag = False
-        for i, letter in enumerate(secret_word):
-            if user_letter == letter:
-                flag = True
-                word_with_lines[i] = letter
-                
-                if "_" not in word_with_lines:
-                    return f"""Congratulations, you guessed the whole word: '[{secret_word}]' 
-you used {attempts - current_attempts} from {attempts} attempts"""  
-        if flag == True:    
-            print(f"""Successful guess 
-the word now is: {show_word_status(word_with_lines)}""") 
-    
-        else:
+        if check_user_guess(user_letter,secret_word):
+            update_masked_word(user_letter,masked_word,secret_word)
+            if check_victory(masked_word):    
+                return f"""Congratulations, you guessed the whole word: '[{secret_word}]' 
+you used {attempts - current_attempts} from {attempts} attempts"""
+            else:
+                            print(f"""Successful guess 
+{show_status_game(masked_word,current_attempts,used_letters)}""")      
+        else:     
             current_attempts -= 1
             print(f"""booz, wrong guess! 
-You have {current_attempts} attempts left
-Status of word is {show_word_status(word_with_lines)}""")
+{show_status_game(masked_word,current_attempts,used_letters)}""")
     return f"You failed, The game is over because you used all attempts the word was: '[{secret_word}]'"    
 
 def menu():
@@ -68,17 +84,15 @@ Welcome to 'guess the word' game
    press 1 to start the game 
    press 2 to exit 
         : """))
-
             match action:
                 case 1:
-                    print(game())
-                    sleep(3)
+                    print(game_management())
+                    sleep(2)
                 case 2:
                     print("bye...")
                     running = False
                 case _:
                      print("Enter 1 or 2 only!")   
-            
         except ValueError:
             print("Enter 1 or 2 only!")
             
